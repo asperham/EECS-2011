31835d8444ce850c8d9fa042c9d2d230b5214c6f
Colin LeDonne <colin.ledonne@hotmail.com>
2023-03-26 06:05:17 -0400

    Add files via upload

---
+++ A3Q2.java
@@ -0,0 +1,44 @@
+/**
+ * EECS 2011 Z, Winter 2023
+ * Assignment 3, Question 2 starter code.
+ * Please read carefully the instructions in the assignment handout
+ * and the starter code.
+ */
+package A3;
+// Do NOT modify the package declaration
+// Do NOT add any import
+/**
+ * Your own implementation of a HashTable class.
+ * Add the appropriate fields and methods you want in order to solve this problem.
+ */
+class HashTable {
+
+    // TODO: implement your own HashTable class
+}
+public class A3Q2 {
+
+    /**
+     * Return the number of unique integer solutions where each integer is in the range [-50, 50].
+     * The equation is given in the handout.
+     * Precondition: A, B, C, D, E, S are integers in the range [-5000, 5000]
+     * Note that the return value of this function is a long integer, not just an int.
+     *
+     * Do NOT modify the signature of this function.
+     */
+    static long solve(int A, int B, int C, int D, int E, int S) {
+
+        // TODO: implement this function
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        // Below are a few test cases.
+        // Printing "true" means the return value is correct.
+        // Each call to solve() must take less than 5 seconds.
+
+        System.out.println(340 == solve(12, 34, 56, 78, 9, 10));
+        System.out.println(16665 == solve(20, -11, -2, 0, 11, -2011));
+        System.out.println(10510100501L == solve(0, 0, 0, 0, 0, 0));
+    }
+}
